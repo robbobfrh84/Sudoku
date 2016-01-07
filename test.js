4586af246cff0f2afdc09c6faf3d6080aff7aaad
robbobfrh84 <bobmain49@gmail.com>
2016-01-07 07:10:06 -0800

    Upload css folder

---
+++ test.js
@@ -0,0 +1,7 @@
+//alert("Help me fix this program!");
+//alert(Can you get this message to appear?);
+document.write("<h2>My first JavaScript program</h2>");
+document.write("<h2>I'm practicing 'debugging'.</h2>");
+for (var i = 1; i <= 5;i++){
+  document.write("<h1>WTF!</h1>"+i);
+}
