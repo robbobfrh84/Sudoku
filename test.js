52536910fdfd80a5ea61fa0ebbbaf6b53b4e2471
robbobfrh84 <bobmain49@gmail.com>
2016-01-07 08:03:17 -0800

    edits

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 //alert("Help me fix this program!");
 //alert(Can you get this message to appear?);
-document.write("<h2>My first JavaScript program</h2>");
-document.write("<h2>I'm practicing 'debugging'.</h2>");
+document.write("<h2>Look Emily!</h2>");
+document.write("<h2>I got it to work! With you help of course ;-)</h2>");
 for (var i = 1; i <= 9;i++){
   document.write("<h1>WTF!</h1>"+i);
 }
