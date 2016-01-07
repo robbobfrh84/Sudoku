ca38ede03132e30c5926775a346c33a29d610a4f
robbobfrh84 <bobmain49@gmail.com>
2016-01-07 07:57:18 -0800

    edit1

--- test.js
+++ test.js
@@ -2,6 +2,6 @@
 //alert(Can you get this message to appear?);
 document.write("<h2>My first JavaScript program</h2>");
 document.write("<h2>I'm practicing 'debugging'.</h2>");
-for (var i = 1; i <= 5;i++){
+for (var i = 1; i <= 9;i++){
   document.write("<h1>WTF!</h1>"+i);
 }
