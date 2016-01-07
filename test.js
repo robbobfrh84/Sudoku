dbdf86e68c821acfd099607b23bae13152737f52
robbobfrh84 <bobmain49@gmail.com>
2016-01-07 07:11:54 -0800

    Revert "Upload css folder"
    This reverts commit 4586af246cff0f2afdc09c6faf3d6080aff7aaad.

--- test.js
+++
@@ -1,7 +0,0 @@
-//alert("Help me fix this program!");
-//alert(Can you get this message to appear?);
-document.write("<h2>My first JavaScript program</h2>");
-document.write("<h2>I'm practicing 'debugging'.</h2>");
-for (var i = 1; i <= 5;i++){
-  document.write("<h1>WTF!</h1>"+i);
-}
