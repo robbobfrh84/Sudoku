64978bb4ecf5d93ca03ff09df0fb9f48eedaa7f3
robbobfrh84 <bobmain49@gmail.com>
2016-01-07 20:40:07 -0800

    poooop

--- test.js
+++ test.js
@@ -3,9 +3,9 @@
 document.write("<h2>Look, Emily!</h2>");
 document.write("<h2>I got it to work! With you help of course ;-) !!!</h2>");
 var x;
-for (var i = 1; i <= 30;i++){
+for (var i = 1; i <= 10;i++){
   x = Math.floor(Math.random() * 100) + 1;
   y = x.toString();
   document.write("<h1>WTF!</h1>"+i);
-  document.write(" "+y); 
+  document.write(" "+y);
 }
