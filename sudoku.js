676bde307a2b70d9fa1da784feb0b1449a57cd7f
robbobfrh84 <bobmain49@gmail.com>
2016-01-24 15:35:53 -0800

    edits

--- sudoku.js
+++ sudoku.js
@@ -10,7 +10,7 @@ newButton.appendChild(document.createTextNode("Medium"));
 bp.insertBefore(newButton, bp.children[1]);
 
 //var easyPuzzle = "5793 1684 4 9782512  4 673  652 3978 3   54   9268 51382 514 96  6 32147 147    5".split('');
-var easyPuzzle = "57932 684643978 512814567 91652 397873819 4 2492687513827514396956832147314769825".split('');
+var easyPuzzle = "57932 684643978251281456739165243978738195462492687513827514396956832147314769825".split('');
 var mediumPuzzle = "3948 251775 149 3   83  964    3 87 8256713494  9 8   5   8 4  18749  2 649215   ".split('');
 var hardPuzzle = "4562  3 7  14   599 21 5  4  46 7598598  2 716   984 3  39    216 82       73194 ".split('');
 var xHardPuzzle = "        13 1      7 5 4  6 13  67    5 3 4      8 5   5 3 8  128             3 8 ".split('');
@@ -50,7 +50,6 @@ function build(blockString){ //places puzzle number blocks and editable
 }
 
 function checkInput(evt, x) {
-  console.log(x);
   var num = parseInt(evt.keyCode);
   event.preventDefault();
   if(num >= 49 && num <= 57){
@@ -81,16 +80,17 @@ function checkCorrect(){
         document.getElementById('blk'+i).setAttribute("contentEditable", false);
       }
     }
-    var congrats = document.createElement("p");
     var minutes = parseInt(seconds/60); seconds -= (minutes*60);
     if (seconds < 10) {
       seconds = "0"+seconds.toString()
     }
-    congrats.innerHTML = "Puzzle Conquered!<br>"+minutes+" min   "+seconds+" sec";
-    congrats.className = "announcement";
-
-    bp.removeChild(bp.children[1]);
-    bp.appendChild(congrats);
+    var timeSave = document.createElement("p");
+    timeSave.innerHTML = +minutes+" min   "+seconds+" sec";
+    timeSave.className = "announcement timer timerz";
+    mainTitle.innerHTML = "Puzzle Conquered!";
+    topDiv.removeChild(topDiv.children[1]);
+    topDiv.insertBefore(timeSave, topDiv.children[1]);
+    stillPlaying = false;
   }
 }
 
@@ -118,40 +118,26 @@ function xHardButton(){
 function menuSwap(){
   topDiv.style.display = "block";
   bp.style.display = "none";
-  // var newGame = document.createElement("button");
-  // newGame.innerHTML = "Start a New Game";
-  // newGame.className = "newGameButton";
-  // topDiv.style.height = "50px";
-  // topDiv.appendChild(newGame);
-  // newGame.onclick = function(){
-  //   menuSwapBack();
-  // }
 }
 
-function menuSwapBack() {
+function newGameWarning(){
   if(confirm("\n!**! WARNING !**!\n\nIf you start a new game you will lose all progress on this Puzzle....\n\n CONINUE?")){
-    while (blkH.hasChildNodes()){
-      blkH.removeChild(blkH.lastChild);
-    }
-    blankBuild();
-    // while (bp.hasChildNodes()){
-    //   bp.removeChild(bp.lastChild);
-    // }
-    topDiv.style.display = "none";
-
-    bp.style.display = "block";
-    // var buttNames = ['Easy', 'Medium','Hard','X-Hard']
-    // for (var i = 0; i < 4; i++){
-    //   var newGame = {}
-    //   newGame[i] = document.createElement("button");
-    //   newGame[i].appendChild(document.createTextNode(buttNames[i]));
-    //   if (i==0){newGame[i].setAttribute("onclick", "easyButton()");}
-    //   if (i==1){newGame[i].setAttribute("onclick", "mediumButton()");}
-    //   if (i==2){newGame[i].setAttribute("onclick", "hardButton()");}
-    //   if (i==3){newGame[i].setAttribute("onclick", "xHardButton()");}
-    //   bp.appendChild(newGame[i]);
-    // }
+    stillPlaying = false;
+    menuSwapBack()
   }
+}
+
+stillPlaying = true; // this disables the user alert if it is FALSE. for when user wins.
+function menuSwapBack() {
+  if (stillPlaying){ newGameWarning();  return; }
+  mainTitle.innerHTML = "Sudoku";
+  while (blkH.hasChildNodes()){
+    blkH.removeChild(blkH.lastChild);
+  }
+  blankBuild();
+  topDiv.style.display = "none";
+  bp.style.display = "block";
+  stillPlaying = true;
 }
 
 //create the function that sets the timer.
@@ -167,6 +153,7 @@ function gameTimer(){
 }
 
 function gameTimerPlace(){
+  topDiv.removeChild(topDiv.lastChild)
   resetInterval();
   seconds = 0;
   var timer = document.createElement("div");
